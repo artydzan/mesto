87bac177d8219d554417ae727397128ffbab7415
artydzan <113125443+artydzan@users.noreply.github.com>
2022-12-07 17:06:18 +0200

    pr5

---
+++ scripts/const.js
@@ -0,0 +1,26 @@
+const initialCards = [
+  {
+    name: 'Бали',
+    link: 'https://images.unsplash.com/photo-1670330883632-819e70d45073?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1035&q=80'
+  },
+  {
+    name: 'Куршская коса',
+    link: 'https://images.unsplash.com/photo-1645127434513-63c301ebf6de?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2533&q=80'
+  },
+  {
+    name: 'Лос-Анджелес',
+    link: 'https://images.unsplash.com/photo-1574910844753-9f49dd2af188?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2071&q=80'
+  },
+  {
+    name: 'Дубровник',
+    link: 'https://images.unsplash.com/photo-1555990793-da11153b2473?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2071&q=80'
+  },
+  {
+    name: 'Амстердам',
+    link: 'https://images.unsplash.com/photo-1512470876302-972faa2aa9a4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'
+  },
+  {
+    name: 'Ватикан',
+    link: 'https://images.unsplash.com/photo-1531572753322-ad063cecc140?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2076&q=80'
+  }
+];
--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,107 @@
-let name = document.querySelector('.profile__name');
-let desctiption = document.querySelector('.profile__description');
-let editButton = document.querySelector('.profile__edit-button');
-let closeButton = document.querySelector('.popup__close-icon');
-let popup = document.querySelector('.popup');
-let formElement = document.querySelector('.form');
-let nameInput = formElement.querySelector('.field__item_value_name');
-let aboutInput = formElement.querySelector('.field__item_value_description');
-function popupOpen() {
-  popup.classList.add('popup__opened');
-  nameInput.value = name.textContent;
-  aboutInput.value = desctiption.textContent;
+const nameProfile = document.querySelector('.profile__name')
+const descriptionProfile = document.querySelector('.profile__description')
+const editProfileBtn = document.querySelector('.profile__edit-button')
+const addProfileBtn = document.querySelector('.profile__add-button')
+const popupBigImg = document.querySelector('.popup__bigimg')
+const popupProfileEdit = document.querySelector('.popup_edit')
+const popupProfileAdd = document.querySelector('.popup_add')
+const closePopupEditBtn = popupProfileEdit.querySelector('.popup__close-icon')
+const closePopupAddBtn = popupProfileAdd.querySelector('.popup__close-icon')
+const closePopupBigBtn = popupBigImg.querySelector('.popup__close-icon')
+const popupBigImage = popupBigImg.querySelector('.popup__image')
+const popupBigTitle = popupBigImg.querySelector('.popup__title')
+const elementsCardContainer = document.querySelector('.elements__list')
+const popupAddCardForm = document.querySelector('.form_add')
+const popupAddInputTitleForm = document.querySelector('.field__item_value_title')
+const popupAddinputUrlForm = document.querySelector('.field__item_value_url')
+const popupEditElementForm = document.querySelector('.form_edit') 
+const popupEditInputNameForm = popupEditElementForm.querySelector('.field__item_value_name')
+const popupEditInputDescriptionForm = popupEditElementForm.querySelector('.field__item_value_description')
+const templatesCard = document
+  .querySelector('#elements-card')
+  .content.querySelector('.element')
+const popupOpening = document.querySelector('.popup')
+
+function openPopup(popup) {
+  popup.classList.add('popup_opened')
 }
 
-function popupClose() {
-  popup.classList.remove('popup__opened');
+function closePopup(popup) {
+  popup.classList.remove('popup_opened')
 }
 
-function formSubmitHandler(evt) {
-  evt.preventDefault();
-  name.textContent = nameInput.value;
-  desctiption.textContent = aboutInput.value;
-  popupClose();
+function submitEditProfileForm(evt) {
+  evt.preventDefault()
+  nameProfile.textContent = popupEditInputNameForm.value 
+  descriptionProfile.textContent = popupEditInputDescriptionForm.value
+  closePopup(popupProfileEdit)
 }
 
-editButton.addEventListener('click', popupOpen);
-closeButton.addEventListener('click', popupClose);
-formElement.addEventListener('submit', formSubmitHandler);
+const handleTemplatesDeleteCard = (event) => {
+  event.target.closest('.element').remove()
+}
+
+const handleTemplatesLikeCard = (event) => {
+  event.target
+    .closest('.element__like')
+    .classList.toggle('element__like_active')
+}
+
+const generateTemplatesCard = (dataCard) => {
+  const newTemplatesCard = templatesCard.cloneNode(true)
+  const titleTemplates = newTemplatesCard.querySelector('.element__title')
+  const imageTemplates = newTemplatesCard.querySelector('.element__pic')
+  titleTemplates.textContent = dataCard.name
+  imageTemplates.src = dataCard.link
+  imageTemplates.alt = dataCard.name
+  
+  const deleteTemplatesBtn = newTemplatesCard.querySelector('.element__trash')
+  deleteTemplatesBtn.addEventListener('click', handleTemplatesDeleteCard)
+  const likeTemplatesBtn = newTemplatesCard.querySelector('.element__like')
+  likeTemplatesBtn.addEventListener('click', handleTemplatesLikeCard)
+
+  const openPopupBigImage = () => {
+    openPopup(popupBigImg)
+    popupBigImage.src = imageTemplates.src
+    popupBigTitle.textContent = dataCard.name
+    popupBigImage.alt = dataCard.name
+  }
+  imageTemplates.addEventListener('click', openPopupBigImage)
+
+  return newTemplatesCard
+}
+const submitAddCardForm = (event) => {
+  event.preventDefault()
+  renderInitialCards({ name: popupAddInputTitleForm.value, link: popupAddinputUrlForm.value })
+  closePopup(popupProfileAdd)
+  popupAddCardForm.reset();
+}
+const renderInitialCards = (dataCard) => {
+  elementsCardContainer.prepend(generateTemplatesCard(dataCard))
+}
+
+initialCards.forEach((dataCard) => {
+  renderInitialCards(dataCard)
+})
+
+popupAddCardForm.addEventListener('submit', submitAddCardForm)
+
+editProfileBtn.addEventListener('click', () => {
+  openPopup(popupProfileEdit)
+  popupEditInputNameForm.value = nameProfile.textContent;
+  popupEditInputDescriptionForm.value = descriptionProfile.textContent;
+})
+
+addProfileBtn.addEventListener('click', () => openPopup(popupProfileAdd))
+closePopupEditBtn.addEventListener('click', () => closePopup(popupProfileEdit))
+
+closePopupAddBtn.addEventListener('click', () => closePopup(popupProfileAdd))
+
+closePopupBigBtn.addEventListener('click', () => {
+  closePopup(popupBigImg);
+  popupBigImage.src = '';
+});
+
+popupEditElementForm.addEventListener('submit', submitEditProfileForm)
+
+popupBigImage.src = '';
